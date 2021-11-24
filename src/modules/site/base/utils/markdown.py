2084a7f13888243b1b27d20278447958a098d8e4
Sam Wachspress <sam.wachspress@u.northwestern.edu>
2021-11-24 04:32:25 -0600

    add external markdown renderer

--- src/modules/site/base/utils/markdown.py
+++ src/modules/site/base/utils/markdown.py
@@ -1,6 +1,12 @@
 import os
 import markdown
+from logzero import logger
 from flask import Markup, render_template_string
+
+from caendr.services.cloud.storage import get_blob
+
+MODULE_SITE_BUCKET_PUBLIC_NAME = os.environ.get('MODULE_SITE_BUCKET_PUBLIC_NAME')
+
 
 def render_markdown(filename, directory="base/static/content/markdown"):
   path = os.path.join(directory, filename)
@@ -8,4 +14,12 @@ def render_markdown(filename, directory="base/static/content/markdown"):
     return ''
   with open(path) as f:
     template = render_template_string(f.read(), **locals())
+    return Markup(markdown.markdown(template))
+
+
+def render_ext_markdown(bucket: str, path: str):
+  blob = get_blob(bucket, path)
+  if blob:
+    md = str(blob.download_as_text(raw_download=True))
+    template = render_template_string(md, **locals())
     return Markup(markdown.markdown(template))
