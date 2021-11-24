f7a60511eb8c92fe15fc9c686af6e12848acea6f
Sam Wachspress <sam.wachspress@u.northwestern.edu>
2021-11-24 04:30:14 -0600

    fix img-thumb-gen regex

--- src/modules/img_thumb_gen/main.py
+++ src/modules/img_thumb_gen/main.py
@@ -1,6 +1,8 @@
 
 import os
 import re
+
+from logzero import logger
 from wand.image import Image
 from google.cloud import storage
 from dotenv import load_dotenv
@@ -19,14 +21,16 @@ if source_path.startswith('/'):
 prefix = '^' + source_path.replace('/','\/') + '\/.*'
 
 def generate_thumbnails(data, context):
-  thumbnail_regex = f"${prefix}\.thumb.(jpg|jpeg)$"
-  image_regex = f"${prefix}\.(jpg|jpeg)$"
+  logger.info(f'Triggered by: bucket:{data["bucket"]}, name:{data["name"]}')
+  thumbnail_regex = f"{prefix}\.thumb.(jpg|jpeg)$"
+  image_regex = f"{prefix}\.(jpg|jpeg)$"
 
   # Only generate thumbnails for matching paths
-  is_image = re.search(image_regex, data['name']) 
+  is_image = re.search(image_regex, data['name'])
   is_thumbnail = re.search(thumbnail_regex, data['name']) 
 
   if (is_image and not is_thumbnail):
+    logger.info(f'Creating new thumbnail for image: {data}')
     # Download the image and resize it
     bucket = client.get_bucket(data['bucket'])
     thumbnail = Image(blob=bucket.get_blob(data['name']).download_as_string())
@@ -37,4 +41,7 @@ def generate_thumbnails(data, context):
     blob_name = path[0] + ".thumb." + path[1]
     thumbnail_blob = bucket.blob(blob_name)
     thumbnail_blob.upload_from_string(thumbnail.make_blob())
-    
+    logger.info(f'Uploaded new thumbnail for image: {blob_name}')
+
+  else:
+    logger.info(f'Triggered but will not create thumbnail: is_image:{is_image} is_thumbnail:{is_thumbnail}')
