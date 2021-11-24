b15a2b0d2ed567b89cd478d7ef01e47fbd408038
Sam Wachspress <sam.wachspress@u.northwestern.edu>
2021-11-24 04:39:30 -0600

    set public bucket default creds

--- tf/caendr/modules/site/cloud-storage.tf
+++ tf/caendr/modules/site/cloud-storage.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "site_bucket_public" {
-    name = var.module_site_vars.bucket_public_name
-    location = upper(var.google_cloud_vars.region)
+  name = var.module_site_vars.bucket_public_name
+  location = upper(var.google_cloud_vars.region)
 }
 
 resource "google_storage_bucket_access_control" "public_rule" {
@@ -9,8 +9,15 @@ resource "google_storage_bucket_access_control" "public_rule" {
   entity = "allUsers"
 }
 
+resource "google_storage_default_object_access_control" "public_rule" {
+  bucket = google_storage_bucket.site_bucket_public.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
 
 resource "google_storage_bucket" "site_bucket_private" {
-    name = var.module_site_vars.bucket_private_name
-    location = upper(var.google_cloud_vars.region)
+  name = var.module_site_vars.bucket_private_name
+  location = upper(var.google_cloud_vars.region)
 }
+
