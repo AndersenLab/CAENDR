0f0ac34ebd43c12bb887b826d54eb48b5eea151f
Sam Wachspress <sam.wachspress@u.northwestern.edu>
2021-12-15 13:10:10 -0600

    add task handler for gene browser tracks

--- src/modules/api/pipeline-task/routes/task.py
+++ src/modules/api/pipeline-task/routes/task.py
@@ -7,14 +7,16 @@ from pipelines.nemascan import start_nemascan_pipeline
 from pipelines.db_op import start_db_op_pipeline
 from pipelines.indel_primer import start_indel_primer_pipeline
 from pipelines.heritability import start_heritability_pipeline
+from pipelines.gene_browser_tracks import start_gene_browser_tracks_pipeline
 
 from caendr.models.datastore.nemascan_mapping import NemascanMapping
 from caendr.models.datastore.database_operation import DatabaseOperation
 from caendr.models.datastore.indel_primer import IndelPrimer
 from caendr.models.datastore.heritability_report import HeritabilityReport
+from caendr.models.datastore.gene_browser_tracks import GeneBrowserTracks
 
 from caendr.models.error import APIBadRequestError, APIInternalError
-from caendr.models.task import NemaScanTask, DatabaseOperationTask, IndelPrimerTask, HeritabilityTask
+from caendr.models.task import NemaScanTask, DatabaseOperationTask, IndelPrimerTask, HeritabilityTask, GeneBrowserTracksTask
 from caendr.models.pub_sub import PubSubAttributes, PubSubMessage, PubSubStatus
 
 from caendr.services.cloud.task import update_task_status, verify_task_headers
@@ -25,6 +27,7 @@ from caendr.services.nemascan_mapping import update_nemascan_mapping_status
 from caendr.services.database_operation import update_db_op_status
 from caendr.services.indel_primer import update_indel_primer_status
 from caendr.services.heritability_report import update_heritability_report_status
+from caendr.services.gene_browser_tracks import update_gene_browser_track_status
 
 from caendr.utils.json import extract_json_payload
 
@@ -32,7 +35,7 @@ INDEL_PRIMER_TASK_QUEUE_NAME = os.environ.get('INDEL_PRIMER_TASK_QUEUE_NAME')
 NEMASCAN_TASK_QUEUE_NAME = os.environ.get('NEMASCAN_TASK_QUEUE_NAME')
 HERITABILITY_TASK_QUEUE_NAME = os.environ.get('HERITABILITY_TASK_QUEUE_NAME')
 MODULE_DB_OPERATIONS_TASK_QUEUE_NAME = os.environ.get('MODULE_DB_OPERATIONS_TASK_QUEUE_NAME')
-
+MODULE_GENE_BROWSER_TRACKS_TASK_QUEUE_NAME = os.environ.get('MODULE_GENE_BROWSER_TRACKS_TASK_QUEUE_NAME')
 
 task_handler_bp = Blueprint('task_bp', __name__)
 
@@ -66,8 +69,12 @@ def handle_task(payload, task_route):
     response = start_heritability_pipeline(task)
   elif task_route == MODULE_DB_OPERATIONS_TASK_QUEUE_NAME:
     task = DatabaseOperationTask(**payload)
-    response = start_db_op_pipeline(task)      
+    response = start_db_op_pipeline(task)
+  elif task_route == MODULE_GENE_BROWSER_TRACKS_TASK_QUEUE_NAME:
+    task = GeneBrowserTracksTask(**payload)
+    response = start_gene_browser_tracks_pipeline(task)    
   
+  logger.debug(task_route)
   status = 'RUNNING'
   operation_name = ''
   try: 
@@ -85,7 +92,9 @@ def handle_task(payload, task_route):
     update_heritability_report_status(task.id, status=status, operation_name=operation_name)
   elif task.kind == DatabaseOperation.kind:
     update_db_op_status(task.id, status=status, operation_name=operation_name)
-  
+  elif task.kind == GeneBrowserTracks.kind:
+    update_gene_browser_track_status(task.id, status=status, operation_name=operation_name)
+
 
 @task_handler_bp.route('/status', methods=['POST'])
 def update_task():
