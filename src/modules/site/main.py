3a0352f63dcc65a253a543f70242a7dc2467ada1
Rodolfo Vieira <rodolfo@northwestern.edu>
2022-01-28 17:35:12 -0600

    Adding new env development-devops. Adding ENV for sentry

--- src/modules/site/main.py
+++ src/modules/site/main.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv
 from application import create_app
+import monitor
 
 dotenv_file = '.env'
 load_dotenv(dotenv_file)
+
+monitor.init_sentry()
 
 # Initialize application
 app = create_app()
