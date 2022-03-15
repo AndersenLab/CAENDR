5386a89dbc03c8174a8e0590eee42815b0feb08f
Rodolfo Vieira <rodolfovieira@gmail.com>
2022-03-14 20:40:52 -0500

    adding support for running ETL with mock data

--- src/pkg/caendr/caendr/services/database_operation.py
+++ src/pkg/caendr/caendr/services/database_operation.py
@@ -24,7 +24,8 @@ DB_OPS = {
   'DROP_AND_POPULATE_WORMBASE_GENES': 'Rebuild wormbase gene table from external sources',
   'DROP_AND_POPULATE_STRAIN_ANNOTATED_VARIANTS': 'Rebuild Strain Annotated Variant table from .csv.gz file',
   'DROP_AND_POPULATE_ALL_TABLES': 'Rebuild All Tables',
-  'ECHO': 'Test ETL'
+  'TEST_ECHO': 'Test ETL - Echo',
+  'TEST_MOCK_DATA': 'Test ETL - Mock Data '
 }
 
 
