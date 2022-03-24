25a9165668ef5494e689a66ff77ece2407b626a3
Rodolfo Vieira <rodolfovieira@gmail.com>
2022-03-23 21:37:29 -0500

    refactoring to use flake8 syntax

--- src/modules/db_operations/main.py
+++ src/modules/db_operations/main.py
@@ -1,26 +1,23 @@
 import os
-from re import T
 import traceback
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from logzero import logger
 from dotenv import load_dotenv
-from logzero import logger
 import time
-import json
 from caendr.services.email import send_email
 from caendr.models.datastore.database_operation import DatabaseOperation
 from caendr.utils import monitor
 from google.cloud import storage
+
+from caendr.services.cloud.postgresql import get_db_conn_uri, get_db_timeout, db
+from caendr.models.error import EnvVarError
+from operations import execute_operation
 
 dotenv_file = '.env'
 load_dotenv(dotenv_file)
 
 monitor.init_sentry("db_operations")
 
-from caendr.services.cloud.postgresql import get_db_conn_uri, get_db_timeout, db, health_database_status
-from caendr.models.error import EnvVarError
-from operations import execute_operation
 
 MODULE_DB_OPERATIONS_BUCKET_NAME = os.environ.get('MODULE_DB_OPERATIONS_BUCKET_NAME')
 ETL_LOGS_BUCKET_NAME = os.environ.get('ETL_LOGS_BUCKET_NAME')
@@ -32,92 +29,96 @@ OPERATION_ID = os.environ.get('OPERATION_ID', None)
 client = storage.Client()
 
 if not DB_OP or not MODULE_DB_OPERATIONS_BUCKET_NAME or not EXTERNAL_DB_BACKUP_PATH:
-  raise EnvVarError()
+    raise EnvVarError()
 
-def etl_operation_append_log(message = ""):
-  if OPERATION_ID is None:
-    logger.warning(f"Unable to update database_operation with id: {OPERATION_ID}")    
-    return
 
-  bucket = client.get_bucket(ETL_LOGS_BUCKET_NAME)
-  filepath = f"logs/etl/{OPERATION_ID}/output"
-  uri = f"gs://{ETL_LOGS_BUCKET_NAME}/{filepath}"
+def etl_operation_append_log(message=""):
+    if OPERATION_ID is None:
+        logger.warning(f"Unable to update database_operation with id: {OPERATION_ID}")
+        return
 
-  CRLF = "\n"
-  blob = bucket.get_blob(filepath)
-  if blob is not None:
-    old_content = blob.download_as_string()
-    new_content = f"{old_content}{CRLF}{message}"
-  else:
-    blob = storage.Blob(filepath, bucket)
-    new_content = f"{message}"
+    bucket = client.get_bucket(ETL_LOGS_BUCKET_NAME)
+    filepath = f"logs/etl/{OPERATION_ID}/output"
+    uri = f"gs://{ETL_LOGS_BUCKET_NAME}/{filepath}"
 
-  blob.upload_from_string(new_content)
+    CRLF = "\n"
+    blob = bucket.get_blob(filepath)
+    if blob is not None:
+        old_content = blob.download_as_string()
+        new_content = f"{old_content}{CRLF}{message}"
+    else:
+        blob = storage.Blob(filepath, bucket)
+        new_content = f"{message}"
 
-  # update db operation object
-  d = DatabaseOperation(OPERATION_ID)
-  d.set_properties(logs=uri)
-  d.save()
+    blob.upload_from_string(new_content)
+
+    # update db operation object
+    d = DatabaseOperation(OPERATION_ID)
+    d.set_properties(logs=uri)
+    d.save()
 
 
 logger.info('Initializing Flask App')
 app = Flask(__name__)
 app.app_context().push()
 app.config['SQLALCHEMY_DATABASE_URI'] = get_db_conn_uri()
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS', 'False').lower() == 'true'
+track_modifications = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS', 'False').lower() == 'true'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = track_modifications
 
 if not os.getenv("MODULE_DB_OPERATIONS_CONNECTION_TYPE"):
-  app.config['SQLALCHEMY_ENGINE_OPTIONS'] = { "pool_pre_ping": True, "pool_recycle": 300 }
-  app.config['SQLALCHEMY_POOL_TIMEOUT'] = get_db_timeout()
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"pool_pre_ping": True, "pool_recycle": 300}
+    app.config['SQLALCHEMY_POOL_TIMEOUT'] = get_db_timeout()
 
 logger.info('Initializing Flask SQLAlchemy')
 db.init_app(app)
 
 
 def run():
-  start = time.perf_counter()
-  use_mock_data = os.getenv('USE_MOCK_DATA', False)
-  text = ""
+    start = time.perf_counter()
+    use_mock_data = os.getenv('USE_MOCK_DATA', False)
+    text = ""
 
-  try:
-    execute_operation(app, db, DB_OP)
-    text = text + f"\n\nStatus: OK"
-    text = text + f"\nOperation: {DB_OP}"
-    text = text + f"\nOperation ID: {OPERATION_ID}"
-    text = text + f"\nEnvironment: { os.getenv('ENV', 'n/a') }"
-  except Exception as e:
-    text = text + f"\nStatus: ERROR"
-    text = text + f"\nOperation: {DB_OP}"
-    text = text + f"\nOperation ID: {OPERATION_ID}"
-    text = text + f"\nEnvironment: { os.getenv('ENV', 'n/a') }"
-    text = text + f"\n\nError: {e}\n{traceback.format_exc()}"
-    logger.error(text)
+    try:
+        execute_operation(app, db, DB_OP)
+        text = text + "\n\nStatus: OK"
+        text = text + f"\nOperation: {DB_OP}"
+        text = text + f"\nOperation ID: {OPERATION_ID}"
+        text = text + f"\nEnvironment: { os.getenv('ENV', 'n/a') }"
+    except Exception as e:
+        text = text + "\nStatus: ERROR"
+        text = text + f"\nOperation: {DB_OP}"
+        text = text + f"\nOperation ID: {OPERATION_ID}"
+        text = text + f"\nEnvironment: { os.getenv('ENV', 'n/a') }"
+        text = text + f"\n\nError: {e}\n{traceback.format_exc()}"
+        logger.error(text)
 
-  elapsed = "{:2f}".format(time.perf_counter() - start)
-  text = text + f"\nProcessed in {elapsed} seconds."
-  text = text + f"\nMock Data: { 'yes' if use_mock_data else 'no' }. (Production should NOT use mock data.)"
+    elapsed = "{:2f}".format(time.perf_counter() - start)
+    text = text + f"\nProcessed in {elapsed} seconds."
+    text = text + f"\nMock Data: { 'yes' if use_mock_data else 'no' }. (Production should NOT use mock data.)"
 
-  log_filepath = "/google/logs/output"
-  try:
-    with open(log_filepath, mode='r') as f:
-      log_data = f.read()
-      text = text + f"\nLOGS: { log_data}"
-  except Exception as e:
-    logger.warning(f"E_UNABLE_TO_ACCESS_LOGS: {e}")
-    text = text + f"\nLOGS: not available"
+    log_filepath = "/google/logs/output"
+    try:
+        with open(log_filepath, mode='r') as f:
+            log_data = f.read()
+            text = text + f"\nLOGS: { log_data}"
+    except Exception as e:
+        logger.warning(f"E_UNABLE_TO_ACCESS_LOGS: {e}")
+        text = text + "\nLOGS: not available"
 
-  etl_operation_append_log(text)
-  logger.info(text)
+    etl_operation_append_log(text)
+    logger.info(text)
 
-  if EMAIL is not None:
-    logger.info(f"Sending email to: {EMAIL}")
-    send_email({"from": "no-reply@elegansvariation.org",
+    if EMAIL is not None:
+        logger.info(f"Sending email to: {EMAIL}")
+        send_email({"from": "no-reply@elegansvariation.org",
                     "to": EMAIL,
                     "subject": f"ETL finished for operation: {DB_OP} in {elapsed} seconds",
-                    "text": text })
+                    "text": text})
+
 
 if __name__ == "__main__":
-  try:    
-    run()
-  except Exception as e:
-    etl_operation_append_log(e)
+    try:
+        run()
+    except Exception as e:
+        logger.error(e)
+        etl_operation_append_log(e)
--- src/modules/db_operations/operations.py
+++ src/modules/db_operations/operations.py
@@ -7,113 +7,119 @@ from caendr.models.sql import WormbaseGene, WormbaseGeneSummary, Strain, Homolog
 from caendr.services.sql.db import (drop_tables,
                                     download_all_external_dbs,
                                     download_external_db,
-                                    backup_external_db,
                                     fetch_internal_db)
-from caendr.services.sql.etl import (load_strains, 
-                                      load_genes_summary, 
-                                      load_genes, 
-                                      load_homologs,
-                                      load_orthologs,
-                                      load_strain_annotated_variants)
-
+from caendr.services.sql.etl import (load_strains,
+                                     load_genes_summary,
+                                     load_genes,
+                                     load_homologs,
+                                     load_orthologs,
+                                     load_strain_annotated_variants)
 
 
 def execute_operation(app, db, DB_OP):
-  WORMBASE_VERSION = os.environ.get('WORMBASE_VERSION')
-  STRAIN_VARIANT_ANNOTATION_VERSION = os.environ.get('STRAIN_VARIANT_ANNOTATION_VERSION')
-  
-  logger.info(f'Executing {DB_OP}: WORMBASE_VERSION:{WORMBASE_VERSION} STRAIN_VARIANT_ANNOTATION_VERSION:{STRAIN_VARIANT_ANNOTATION_VERSION}')
+    WORMBASE_VERSION = os.environ.get('WORMBASE_VERSION')
+    STRAIN_VARIANT_ANNOTATION_VERSION = os.environ.get('STRAIN_VARIANT_ANNOTATION_VERSION')
 
-  if DB_OP == 'DROP_AND_POPULATE_ALL_TABLES':
-    if not WORMBASE_VERSION or not STRAIN_VARIANT_ANNOTATION_VERSION:
-      raise EnvVarError()
-    drop_and_populate_all_tables(app, db, WORMBASE_VERSION, STRAIN_VARIANT_ANNOTATION_VERSION)
-    
-  elif DB_OP == 'DROP_AND_POPULATE_STRAINS':
-    drop_and_populate_strains(app, db)
-    
-  elif DB_OP == 'DROP_AND_POPULATE_WORMBASE_GENES':
-    if not WORMBASE_VERSION:
-      raise EnvVarError()
-    drop_and_populate_wormbase_genes(app, db, WORMBASE_VERSION)
+    logger.info(f'Executing {DB_OP}')
+    logger.info(f'WORMBASE_VERSION:{WORMBASE_VERSION}')
+    logger.info(f'STRAIN_VARIANT_ANNOTATION_VERSION:{STRAIN_VARIANT_ANNOTATION_VERSION}')
 
-  elif DB_OP == 'DROP_AND_POPULATE_STRAIN_ANNOTATED_VARIANTS':
-    if not STRAIN_VARIANT_ANNOTATION_VERSION:
-      raise EnvVarError()
-    drop_and_populate_strain_annotated_variants(app, db, STRAIN_VARIANT_ANNOTATION_VERSION)
-  
-  elif DB_OP == 'TEST_ECHO':
-    result, message = health_database_status()
-    if not result:
-      raise Exception(f"DB Connection is: { ('OK' if result else 'ERROR') }. {message}")
+    if DB_OP == 'DROP_AND_POPULATE_ALL_TABLES':
+        drop_and_populate_all_tables(app, db, WORMBASE_VERSION, STRAIN_VARIANT_ANNOTATION_VERSION)
 
-  elif DB_OP == 'TEST_MOCK_DATA':
-    os.environ["USE_MOCK_DATA"] = "1"
-    os.environ["MODULE_DB_OPERATIONS_CONNECTION_TYPE"] = "memory"
-    logger.info("Using MOCK DATA")
-    drop_and_populate_all_tables(app, db, WORMBASE_VERSION, STRAIN_VARIANT_ANNOTATION_VERSION)
+    elif DB_OP == 'DROP_AND_POPULATE_STRAINS':
+        drop_and_populate_strains(app, db)
+
+    elif DB_OP == 'DROP_AND_POPULATE_WORMBASE_GENES':
+        drop_and_populate_wormbase_genes(app, db, WORMBASE_VERSION)
+
+    elif DB_OP == 'DROP_AND_POPULATE_STRAIN_ANNOTATED_VARIANTS':
+        drop_and_populate_strain_annotated_variants(app, db, STRAIN_VARIANT_ANNOTATION_VERSION)
+
+    elif DB_OP == 'TEST_ECHO':
+        result, message = health_database_status()
+        if not result:
+            raise Exception(f"DB Connection error: {message}")
+
+    elif DB_OP == 'TEST_MOCK_DATA':
+        test_mock_data(app, db, WORMBASE_VERSION, STRAIN_VARIANT_ANNOTATION_VERSION)
+        return
 
 
 def drop_and_populate_strains(app, db):
-  drop_tables(app, db, tables=[Strain.__table__])
-  load_strains(db)
+    drop_tables(app, db, tables=[Strain.__table__])
+    load_strains(db)
+
 
 def drop_and_populate_wormbase_genes(app, db, wb_ver: str):
-  logger.info(f"Running Drop and Populate wormbase genes with version: {wb_ver}")
-  gene_gff_fname = download_external_db('GENE_GFF_URL', wb_ver=wb_ver)
-  gene_gtf_fname = download_external_db('GENE_GTF_URL', wb_ver=wb_ver)
-  gene_ids_fname = download_external_db('GENE_IDS_URL', wb_ver=wb_ver)
-  homologene_fname = download_external_db('HOMOLOGENE_URL')
-  ortholog_fname = download_external_db('ORTHOLOG_URL')
-  
-  drop_tables(app, db, tables=[Homolog.__table__, WormbaseGene.__table__])
-  drop_tables(app, db, tables=[WormbaseGeneSummary.__table__])
-  load_genes_summary(db, gene_gff_fname)
-  load_genes(db, gene_gtf_fname, gene_ids_fname)
-  load_homologs(db, homologene_fname)
-  load_orthologs(db, ortholog_fname)
+    if not wb_ver:
+        raise EnvVarError()
+    logger.info(f"Running Drop and Populate wormbase genes with version: {wb_ver}")
+    gene_gff_fname = download_external_db('GENE_GFF_URL', wb_ver=wb_ver)
+    gene_gtf_fname = download_external_db('GENE_GTF_URL', wb_ver=wb_ver)
+    gene_ids_fname = download_external_db('GENE_IDS_URL', wb_ver=wb_ver)
+    homologene_fname = download_external_db('HOMOLOGENE_URL')
+    ortholog_fname = download_external_db('ORTHOLOG_URL')
+
+    drop_tables(app, db, tables=[Homolog.__table__, WormbaseGene.__table__])
+    drop_tables(app, db, tables=[WormbaseGeneSummary.__table__])
+    load_genes_summary(db, gene_gff_fname)
+    load_genes(db, gene_gtf_fname, gene_ids_fname)
+    load_homologs(db, homologene_fname)
+    load_orthologs(db, ortholog_fname)
 
 
 def drop_and_populate_strain_annotated_variants(app, db, sva_ver: str):
-  sva_fname = fetch_internal_db('SVA_CSVGZ_URL', sva_ver=sva_ver)
-  db.session.commit()
-  logger.info(f"Dropping table...")
-  drop_tables(app, db, tables=[StrainAnnotatedVariant.__table__])
-  logger.info("Loading strain annotated variants...")
-  load_strain_annotated_variants(db, sva_fname)
+    if not sva_ver:
+        raise EnvVarError()
+    sva_fname = fetch_internal_db('SVA_CSVGZ_URL', sva_ver=sva_ver)
+    db.session.commit()
+    logger.info("Dropping table...")
+    drop_tables(app, db, tables=[StrainAnnotatedVariant.__table__])
+    logger.info("Loading strain annotated variants...")
+    load_strain_annotated_variants(db, sva_fname)
+
+
+def test_mock_data(app, db, wb_ver: str, sva_ver: str):
+    os.environ["USE_MOCK_DATA"] = "1"
+    os.environ["MODULE_DB_OPERATIONS_CONNECTION_TYPE"] = "memory"
+    logger.info("Using MOCK DATA")
+    drop_and_populate_all_tables(app, db, wb_ver, sva_ver)
 
 
 def drop_and_populate_all_tables(app, db, wb_ver: str, sva_ver: str):
-  logger.info(f'Dropping and populating all tables - WORMBASE_VERSION: {wb_ver} STRAIN_VARIANT_ANNOTATION_VERSION: {sva_ver}')
-  logger.info("[1/8] Downloading databases...eta ~0:15")
-  filenames = download_all_external_dbs(wb_ver)
-  gene_gff_fname = filenames['GENE_GFF_URL']
-  gene_gtf_fname = filenames['GENE_GTF_URL']
-  gene_ids_fname = filenames['GENE_IDS_URL']
-  homologene_fname = filenames['HOMOLOGENE_URL']
-  ortholog_fname = filenames['ORTHOLOG_URL']
-  
-  filenames['SVA_CSVGZ_URL'] = fetch_internal_db('SVA_CSVGZ_URL', sva_ver)
-  sva_fname = filenames['SVA_CSVGZ_URL']
+    if not wb_ver or not sva_ver:
+        raise EnvVarError()
+    logger.info("Dropping and populating all tables")
+    logger.info('WORMBASE_VERSION: {wb_ver} STRAIN_VARIANT_ANNOTATION_VERSION: {sva_ver}')
+    logger.info("[1/8] Downloading databases...eta ~0:15")
+    filenames = download_all_external_dbs(wb_ver)
+    gene_gff_fname = filenames['GENE_GFF_URL']
+    gene_gtf_fname = filenames['GENE_GTF_URL']
+    gene_ids_fname = filenames['GENE_IDS_URL']
+    homologene_fname = filenames['HOMOLOGENE_URL']
+    ortholog_fname = filenames['ORTHOLOG_URL']
 
-  logger.info("[2/8] Dropping tables...eta ~0:01")
-  drop_tables(app, db)
+    filenames['SVA_CSVGZ_URL'] = fetch_internal_db('SVA_CSVGZ_URL', sva_ver)
+    sva_fname = filenames['SVA_CSVGZ_URL']
 
-  logger.info("[3/8] Load Strains...eta ~0:24")
-  load_strains(db)
+    logger.info("[2/8] Dropping tables...eta ~0:01")
+    drop_tables(app, db)
 
-  logger.info("[4/8] Load genes summary...eta ~3:15")
-  load_genes_summary(db, gene_gff_fname)
+    logger.info("[3/8] Load Strains...eta ~0:24")
+    load_strains(db)
 
-  logger.info("[5/8] Load genes...eta ~12:37")
-  load_genes(db, gene_gtf_fname, gene_ids_fname)
+    logger.info("[4/8] Load genes summary...eta ~3:15")
+    load_genes_summary(db, gene_gff_fname)
 
-  logger.info("[6/8] Load Homologs...eta ~3:10")
-  load_homologs(db, homologene_fname)
+    logger.info("[5/8] Load genes...eta ~12:37")
+    load_genes(db, gene_gtf_fname, gene_ids_fname)
 
-  logger.info("[7/8] Load Horthologs...eta ~17:13")
-  load_orthologs(db, ortholog_fname)
+    logger.info("[6/8] Load Homologs...eta ~3:10")
+    load_homologs(db, homologene_fname)
 
-  logger.info("[8/8] Load Strains Annotated Variants...eta ~26:47")
-  load_strain_annotated_variants(db, sva_fname)
-  
+    logger.info("[7/8] Load Horthologs...eta ~17:13")
+    load_orthologs(db, ortholog_fname)
+
+    logger.info("[8/8] Load Strains Annotated Variants...eta ~26:47")
+    load_strain_annotated_variants(db, sva_fname)
