78998bec19f97dbd36b8adc90cb8490346cfbc63
Rodolfo Vieira <rodolfovieira@gmail.com>
2022-07-28 17:24:03 -0500

    Adding support for query param for wormbase_version override. Adding route with suffix '/' Selecting the latest dataset release version when no dataset release version is included in the URL path, or when it is included and it does not exist.

--- src/modules/site/base/views/data/gene_browser.py
+++ src/modules/site/base/views/data/gene_browser.py
@@ -1,6 +1,5 @@
 import re
 from caendr.models.datastore.dataset_release import DatasetRelease
-from caendr.models.datastore.settings import Settings
 from flask import (render_template,
                     Blueprint, 
                     request,
@@ -21,45 +20,40 @@ def is_valid_release_version(value = None):
     return False
   return value.isdigit()
 
+def is_valid_wormbase_version(value = None):
+  if value is None:
+    return False
+  regex = r"^ws[0-9]+$"
+  return re.match(regex, value, flags=re.IGNORECASE) is not None
+
 def get_dataset_release_or_latest(release_version = None):
   if release_version is None or not is_valid_release_version(release_version):
     return get_latest_dataset_release_version()
-  return get_dataset_release(release_version)
+  dataset_release = get_dataset_release(release_version)
+  if dataset_release is not None:
+    dataset_release
+  return get_latest_dataset_release_version()
 
 
 @gene_browser_bp.route('/gbrowser')
+@gene_browser_bp.route('/gbrowser/')
 @gene_browser_bp.route('/gbrowser/<release_version>')
 @gene_browser_bp.route('/gbrowser/<release_version>/<region>')
 @gene_browser_bp.route('/gbrowser/<release_version>/<region>/<query>')
 @cache.memoize(60*60)
 def gbrowser(release_version=None, region="III:11746923-11750250", query=None):
   dataset_release = get_dataset_release_or_latest(release_version)
+
+  wormbase_version_override = request.args.get('wormbase_version', None)
+  if (wormbase_version_override is not None) and is_valid_wormbase_version(wormbase_version_override):
+    wormbase_version = wormbase_version_override.upper()
+  else:
+    wormbase_version = dataset_release.wormbase_version    
+        
   
-  # TODO: Do a look up on release_version if the parameter is not None.
-  # Extract .version and .wormbase_version from the release_version object 
-  # (DataRelease object)
-
-  base_settings = Settings("base_settings")
-  if not base_settings._exists:
-    base_settings.save()
-
-  gene_browser_settings = Settings("settings#gene-browser")
-  gene_browser_settings.__dict__.update(parent = base_settings)
-  gene_browser_settings.save()
-
-  
-  if not gene_browser_settings._exists:
-    print("creating settings")
-    gene_browser_settings.save()
-
-  # TODO: REMOVE TEMP ASSIGNMENTs
-  # release_version = '20210121'
-  # wormbase_version = 'WS276'
-  # wormbase_version = 'WS283'
   track_url_prefix = f'//storage.googleapis.com/elegansvariation.org/browser_tracks'
   bam_bai_url_prefix = f'//storage.googleapis.com/elegansvariation.org/bam'
   dataset_release_prefix = f'//storage.googleapis.com/elegansvariation.org/releases'
-  
   
   VARS = {'title': f"Genome Browser",
           'DATASET_RELEASE': int(dataset_release.version),
@@ -70,7 +64,7 @@ def gbrowser(release_version=None, region="III:11746923-11750250", query=None):
             "title": "Data", 
             "url": url_for('data.landing')
           },
-          'wormbase_version': dataset_release.wormbase_version,
+          'wormbase_version': wormbase_version,
           'release_version': int(dataset_release.version),
           'track_url_prefix': track_url_prefix,
           'bam_bai_url_prefix': bam_bai_url_prefix,
