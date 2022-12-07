b627334f65abb6988fd929406e15b90baa20a19e
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2022-12-07 13:32:14 -0600

    Pass kwargs to fetch_external_db

--- src/pkg/caendr/caendr/services/sql/dataset/_db_external.py
+++ src/pkg/caendr/caendr/services/sql/dataset/_db_external.py
@@ -6,20 +6,21 @@ from caendr.models.error import BadRequestError
 from caendr.utils.file import download_file
 
 
-def prefetch_all_external_dbs(self, use_cache: bool = True):
+def prefetch_all_external_dbs(self, **kwargs):
     '''
       Downloads all external DB files and saves them locally.
+      Accepts all keyword args of fetch_external_db, except species_name.
     '''
     logger.info('Downloading All External DBs...')
 
     # Download all files that depend on species
     for species_name in self.species_list.keys():
         for url_template_name in self.specific_template_names:
-            self.fetch_external_db(url_template_name, species_name, use_cache=use_cache)
+            self.fetch_external_db(url_template_name, species_name, **kwargs)
 
     # Download all files that don't depend on species
     for url_template_name in self.generic_template_names:
-        self.fetch_external_db(url_template_name, use_cache=use_cache)
+        self.fetch_external_db(url_template_name, None, **kwargs)
 
     logger.info('Done Downloading All External Data.')
 
@@ -29,6 +30,7 @@ def fetch_external_db(self, db_url_name: str, species_name: str = None, use_cach
       fetch_external_db [Downloads an external database file and stores it locally.]
         Args:
           db_url_name (str): [Name used as the key for the Dict of URLs.]
+        Keyword Args:
           species_name (str, optional): [Name of species to retrieve DB file for. Defaults to None. Optional, but must be provided for certain URLs.]
           use_cache (bool, optional): [Whether to use a local copy if it exists (True), or force a re-download of the file (False). Defaults to True.]
           unzip (bool, optional): [Whether to unzip a .gz file. Defaults to False.]
@@ -74,71 +76,63 @@ def fetch_external_db(self, db_url_name: str, species_name: str = None, use_cach
 
 ## Fetch specific URLs ##
 
-def fetch_gene_gtf_db(self, species, use_cache: bool = True):
+def fetch_gene_gtf_db(self, species, **kwargs):
     '''
-      Fetches WormBase gene .gtf.gz file.
+      Fetches WormBase gene GTF file. Accepts all keyword args of fetch_external_db, except species_name.
         Args:
           species (str): [Name of species to retrieve DB file for.]
-          use_cache (bool, optional): [Whether to use a local copy if it exists (True), or force a re-download of the file (False). Defaults to True.]
         Returns:
           gene_gtf_fname (str): [path of downloaded wormbase gene gtf.gz file]
     '''
-    return self.fetch_external_db('GENE_GTF_URL', species, use_cache=use_cache)
+    return self.fetch_external_db('GENE_GTF_URL', species, **kwargs)
 
 
-def fetch_gene_gff_db(self, species, use_cache: bool = True):
+def fetch_gene_gff_db(self, species, **kwargs):
     '''
-      Fetches WormBase gene .gff file.
+      Fetches WormBase gene GFF file. Accepts all keyword args of fetch_external_db, except species_name.
         Args:
           species (str): [Name of species to retrieve DB file for.]
-          use_cache (bool, optional): [Whether to use a local copy if it exists (True), or force a re-download of the file (False). Defaults to True.]
         Returns:
           gene_gff_fname (str): [Path of downloaded wormbase gene gff file.]
     '''
-    return self.fetch_external_db('GENE_GFF_URL', species, use_cache=use_cache)
+    return self.fetch_external_db('GENE_GFF_URL', species, **kwargs)
 
 
-def fetch_gene_ids_db(self, species, use_cache: bool = True):
+def fetch_gene_ids_db(self, species, **kwargs):
     '''
-      Fetches WormBase gene IDs file.
+      Fetches WormBase gene IDs file. Accepts all keyword args of fetch_external_db, except species_name.
         Args:
           species (str): [Name of species to retrieve DB file for.]
-          use_cache (bool, optional): [Whether to use a local copy if it exists (True), or force a re-download of the file (False). Defaults to True.]
         Returns:
           gene_ids_fname (str): [path of downloaded wormbase gene IDs file]
     '''
-    return self.fetch_external_db('GENE_IDS_URL', species, use_cache=use_cache)
+    return self.fetch_external_db('GENE_IDS_URL', species, **kwargs)
 
 
-def fetch_ortholog_db(self, species, use_cache: bool = True):
+def fetch_ortholog_db(self, species, **kwargs):
     '''
-      Fetches WormBase orthologs file.
+      Fetches WormBase orthologs file. Accepts all keyword args of fetch_external_db, except species_name.
         Args:
           species (str): [Name of species to retrieve DB file for.]
-          use_cache (bool, optional): [Whether to use a local copy if it exists (True), or force a re-download of the file (False). Defaults to True.]
         Returns:
           ortholog_fname (str): [path of downloaded wormbase homologs file]
     '''
-    return self.fetch_external_db('ORTHOLOG_URL', species, use_cache=use_cache)
+    return self.fetch_external_db('ORTHOLOG_URL', species, **kwargs)
 
 
-def fetch_homologene_db(self, use_cache: bool = True):
+def fetch_homologene_db(self, **kwargs):
     '''
-      Fetches homologene file.
-        Args:
-          use_cache (bool, optional): [Whether to use a local copy if it exists (True), or force a re-download of the file (False). Defaults to True.]
+      Fetches homologene file. Accepts all keyword args of fetch_external_db, except species_name.
         Returns:
           homologene_fname (str): [path of downloaded homologene file]
     '''
-    return self.fetch_external_db('HOMOLOGENE_URL', use_cache=use_cache)
+    return self.fetch_external_db('HOMOLOGENE_URL', None, **kwargs)
 
 
-def fetch_taxon_id_db(self, use_cache: bool = True):
+def fetch_taxon_id_db(self, **kwargs):
     '''
-      Fetches taxonomic IDs file.
-        Args:
-          use_cache (bool, optional): [Whether to use a local copy if it exists (True), or force a re-download of the file (False). Defaults to True.]
+      Fetches taxonomic IDs file. Accepts all keyword args of fetch_external_db, except species_name.
         Returns:
           taxon_id_fname (str): [path of downloaded taxonomic IDs file]
     '''
-    return self.fetch_external_db('TAXON_ID_URL', use_cache=use_cache)
+    return self.fetch_external_db('TAXON_ID_URL', None, **kwargs)
