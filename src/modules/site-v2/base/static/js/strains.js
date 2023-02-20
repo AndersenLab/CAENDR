74170baee6d2194fb74ed05e7e671f38436769c6
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2023-02-20 14:29:25 -0600

    Move update strain dropdown to JS file

--- src/modules/site-v2/base/static/js/strains.js
+++ src/modules/site-v2/base/static/js/strains.js
@@ -22,3 +22,29 @@ function filter_strains(species_id, search_terms) {
     }
   }
 }
+
+
+function update_strain_dropdown(id, strains, species, initial_value = null) {
+
+  // Clear the specified dropdown
+  $(id).empty();
+
+  // If strains provided, fill out the dropdown
+  if (strains[species['name']]) {
+
+    // Create an option for each strain
+    strains[species['name']].forEach((val) => {
+      $(id).append( $("<option />").val(val).text(val) );
+    });
+
+    // If an initial default val is provided, select it
+    if (initial_value) {
+      $(id).val(initial_value);
+    }
+  }
+
+  // If no strains found, print a warning message
+  else {
+    console.warn(`Couldn't find strains for species ${species['name']}`);
+  }
+}
