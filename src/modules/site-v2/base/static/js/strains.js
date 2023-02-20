56556e0984cf2f35c1d6f7ed4eac9bf79afb6a42
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2023-02-20 14:11:54 -0600

    Fix indentation

--- src/modules/site-v2/base/static/js/strains.js
+++ src/modules/site-v2/base/static/js/strains.js
@@ -1,24 +1,24 @@
 function filter_strains(species_id, search_terms) {
 
-    // Hide all strains
-    $('.strain-entry-container').hide();
-  
-    // If no species ID provided
-    if (species_id) {
-  
-      // If search string(s) provided, show matching strains
-      if (search_terms) {
-        search_terms.forEach(function(r) {
-          var rex = new RegExp(r, "i");
-          $(`.strain-entry-container-${ species_id }`).filter(function() {
-            return rex.test($(this).text());
-          }).show();
-        })
-      }
-  
-      // Otherwise, show all strains matching given species
-      else {
-        $(`.strain-entry-container-${ species_id }`).show()
-      }
+  // Hide all strains
+  $('.strain-entry-container').hide();
+
+  // If no species ID provided
+  if (species_id) {
+
+    // If search string(s) provided, show matching strains
+    if (search_terms) {
+      search_terms.forEach(function(r) {
+        var rex = new RegExp(r, "i");
+        $(`.strain-entry-container-${ species_id }`).filter(function() {
+          return rex.test($(this).text());
+        }).show();
+      })
+    }
+
+    // Otherwise, show all strains matching given species
+    else {
+      $(`.strain-entry-container-${ species_id }`).show()
     }
   }
+}
