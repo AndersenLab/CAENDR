fdc46c72f9411734c189cd5ea3f27f3674e806b3
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2023-03-07 12:54:05 -0600

    Add type hints & id validation

--- src/modules/site-v2/base/static/js/strains.js
+++ src/modules/site-v2/base/static/js/strains.js
@@ -1,4 +1,4 @@
-function filter_strains(species_id, search_terms) {
+function filter_strains(species_id='', search_terms=[]) {
 
   // Hide all strains
   $('.strain-entry-container').hide();
@@ -7,7 +7,7 @@ function filter_strains(species_id, search_terms) {
   if (species_id) {
 
     // If search string(s) provided, show matching strains
-    if (search_terms) {
+    if (search_terms.length > 0) {
       search_terms.forEach(function(r) {
         var rex = new RegExp(r, "i");
         $(`.strain-entry-container-${ species_id }`).filter(function() {
@@ -24,27 +24,34 @@ function filter_strains(species_id, search_terms) {
 }
 
 
-function update_strain_dropdown(id, strains, species, initial_value = null) {
+function update_strain_dropdown(id='', strains={}, species={}, initial_value=null) {
+
+  // Ensure dropdown element exists
+  if (id === '' || !document.getElementById(id)) {
+    throw new Error(`Could not find strain dropdown with id "${id}".`);
+  }
+
+  const selector = '#' + id;
 
   // Clear the specified dropdown
-  $(id).empty();
+  $(selector).empty();
 
   // If strains provided, fill out the dropdown
   if (strains[species['name']]) {
 
     // Create an option for each strain
     strains[species['name']].forEach((val) => {
-      $(id).append( $("<option />").val(val).text(val) );
+      $(selector).append( $("<option />").val(val).text(val) );
     });
 
     // If an initial default val is provided, select it
-    if (initial_value) {
-      $(id).val(initial_value);
+    if (initial_value !== null) {
+      $(selector).val(initial_value);
     }
   }
 
-  // If no strains found, print a warning message
+  // If no strains found, throw an error
   else {
-    console.warn(`Couldn't find strains for species ${species['name']}`);
+    throw new Error(`Couldn't find strains for species ${species['name']}`);
   }
 }
