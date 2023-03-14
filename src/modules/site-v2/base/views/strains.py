975f72622c1686dd01334821a5071cd6b93fde13
Orzu Tursunova <orzut@github.com>
2023-03-14 16:11:22 -0500

    moved Order pages under Request-strains

--- src/modules/site-v2/base/views/strains.py
+++ src/modules/site-v2/base/views/strains.py
@@ -17,8 +17,25 @@ from caendr.api.strain import get_strains, query_strains, get_strain_sets, get_s
 from caendr.models.sql import Strain
 from caendr.utils.json import dump_json
 
+"""
+Author: Daniel E. Cook
 
-strains_bp = Blueprint('request-strains',
+These views handle strain orders
+
+"""
+import uuid
+from datetime import datetime, timezone
+from flask import render_template, request, url_for, redirect, Blueprint, abort, flash
+
+from config import config
+from base.forms import OrderForm
+from base.utils.auth import jwt_required, get_current_user
+
+from caendr.services.email import send_email, ORDER_SUBMISSION_EMAIL_TEMPLATE
+from caendr.services.cloud.sheets import add_to_order_ws, lookup_order
+
+
+strains_bp = Blueprint('request_strains',
                         __name__,
                         template_folder='templates')
 #
@@ -131,3 +148,64 @@ def strains_catalog():
     strain_sets = get_strain_sets()
     return render_template('strain/catalog.html', **locals())
 
+#
+# Strain Ordering Pages
+#
+
+@strains_bp.route('/checkout', methods=['GET', 'POST'])
+@jwt_required(optional=True)
+def order_page():
+  """ This view handles the order page """
+  form = OrderForm()
+  user = get_current_user()
+  if user and hasattr(user, 'email') and not form.email.data:
+    form.email.data = user.email
+
+  # Fetch items
+  items = form.items.data
+  
+  disable_parent_breadcrumb = True
+  title = "Order"
+
+  if (len(items) == 0):
+    flash("You must select strains/sets from the catalog", 'error')
+    return redirect(url_for("request_strains.strains_catalog"))
+
+  # Is the user coming from the catalog?
+  user_from_catalog = request.form.get('from_catalog') == "true"
+
+  # When the user confirms their order it is processed below.
+  if user_from_catalog is False and form.validate_on_submit():
+    order_obj = {'total': form.total,
+                  'date': datetime.now(timezone.utc).date().isoformat(),
+                  'is_donation': False}
+    order_obj.update(form.data)
+    order_obj['phone'] = order_obj['phone'].strip("+")
+    order_obj['items'] = '\n'.join(sorted([u"{}:{}".format(k, v) for k, v in form.item_price()]))
+    order_obj['invoice_hash'] = str(uuid.uuid4()).split("-")[0]
+    order_obj["order_confirmation_link"] = url_for('request_strains.order_confirmation', invoice_hash=order_obj['invoice_hash'], _external=True)
+    send_email({"from": "no-reply@elegansvariation.org",
+                "to": [order_obj["email"]],
+                "cc": config.get("CC_EMAILS"),
+                "subject": "CeNDR Order #" + str(order_obj["invoice_hash"]),
+                "text": ORDER_SUBMISSION_EMAIL_TEMPLATE.format(**order_obj)})
+
+    # Save to google sheet
+    add_to_order_ws(order_obj)
+    flash("Thank you for submitting your order! Please follow the instructions below to complete your order.", 'success')
+    return redirect(url_for("request_strains.order_confirmation", invoice_hash=order_obj['invoice_hash']), code=302)
+  return render_template('order/order.html', **locals())
+
+
+@strains_bp.route("/checkout/confirmation/<invoice_hash>", methods=['GET', 'POST'])
+def order_confirmation(invoice_hash):
+  order_obj = lookup_order(invoice_hash)
+  if order_obj:
+    order_obj["items"] = {x.split(":")[0]: float(x.split(":")[1])
+                          for x in order_obj['items'].split("\n")}
+    if order_obj is None:
+      abort(404)
+    title = f"Invoice {order_obj['invoice_hash']}"
+    return render_template('order/order_confirm.html', **locals())
+  else:
+    abort(404)
