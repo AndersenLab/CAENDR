9d7159b6174100f2f8929a643c5930bef9deb33f
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2023-03-28 15:11:10 -0500

    Disable "Choose" option in species selector

--- src/modules/site-v2/templates/_scripts/utils.js
+++ src/modules/site-v2/templates/_scripts/utils.js
@@ -2,8 +2,8 @@ function can_be_disabled(tag) {
   return ['button', 'fieldset', 'input', 'optgroup', 'option', 'select', 'textarea'].includes(tag);
 }
 
-function toggle_input(id, val) {
-  const el = document.getElementById(id);
+function toggle_input(idOrEl, val) {
+  const el = typeof idOrEl === 'string' ? document.getElementById(idOrEl) : idOrEl;
   const tag = el.tagName.toLowerCase();
 
   // If element supports 'disabled' property, set that (incl. ARIA)
