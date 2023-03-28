e5483b9a69f70b4096adde75193215977d773492
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2023-03-28 10:38:51 -0500

    Handle non-disable-able elements in toggle_input

--- src/modules/site-v2/templates/_scripts/utils.js
+++ src/modules/site-v2/templates/_scripts/utils.js
@@ -1,5 +1,19 @@
+function can_be_disabled(tag) {
+  return ['button', 'fieldset', 'input', 'optgroup', 'option', 'select', 'textarea'].includes(tag);
+}
+
 function toggle_input(id, val) {
   const el = document.getElementById(id);
-  el.disabled     = !val;
-  el.ariaDisabled = !val;
+  const tag = el.tagName.toLowerCase();
+
+  // If element supports 'disabled' property, set that (incl. ARIA)
+  if (can_be_disabled(tag)) {
+    el.disabled     = !val;
+    el.ariaDisabled = !val;
+  }
+
+  // If not, set its background color to mimic a disabled element
+  else {
+    el.style.backgroundColor = `var(--${ val ? 'enabled' : 'disabled' }-color)`;
+  }
 }
