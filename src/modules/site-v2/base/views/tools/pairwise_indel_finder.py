AndersenLab/CAENDR

commit f918dc969716c96e4ee2b86fa04ebe21763e70dc

Use TaskStatus instead of raw strings

src/modules/site-v2/base/views/tools/pairwise_indel_finder.py
@@ -10,6 +10,7 @@ from base.utils.tools import lookup_report, try_submit
 from caendr.models.datastore.browser_track import BrowserTrack, BrowserTrackDefault
 from caendr.models.datastore import SPECIES_LIST, IndelPrimer
 from caendr.models.error import NotFoundError, NonUniqueEntity, ReportLookupError, EmptyReportDataError, EmptyReportResultsError, UnfinishedReportError
+from caendr.models.task import TaskStatus
 from caendr.services.dataset_release import get_browser_tracks_path
 from caendr.utils.constants import CHROM_NUMERIC
 
@@ -230,7 +231,7 @@ def report(id, filename = None):
 
     # Results file exists, but is empty - error
     except EmptyReportResultsError:
-      report.status = 'ERROR'
+      report.status = TaskStatus.ERROR
       report.save()
       return abort(404, description="Pairwise indel finder report not found")
 
@@ -249,8 +250,8 @@ def report(id, filename = None):
     # Update indel primer entity
     # TODO: Is this the right time/place for this?
     if ready:
-      if report['status'] != 'ERROR':
-        report['status'] = 'COMPLETE'
+      if report['status'] != TaskStatus.ERROR:
+        report['status'] = TaskStatus.COMPLETE
       report.empty = result is None
       report.save()
 

src/modules/site-v2/base/utils/tools.py
@@ -12,6 +12,7 @@ from caendr.models.error import (
     FileUploadError,
     ReportLookupError,
 )
+from caendr.models.task import TaskStatus
 from caendr.services.tools import submit_job
 from caendr.utils.data import unique_id
 
@@ -94,7 +95,7 @@ def try_submit(EntityClass, user, data, no_cache):
       'succeeded': True,
       'cached':    True,
       'same_user': True,
-      'ready':     ex.args[0]['status'] == "COMPLETE",
+      'ready':     ex.args[0]['status'] == TaskStatus.COMPLETE,
       'data_hash': ex.args[0].data_hash,
       'id':        ex.args[0].id,
     }
@@ -106,7 +107,7 @@ def try_submit(EntityClass, user, data, no_cache):
       'succeeded': True,
       'cached':    True,
       'same_user': False,
-      'ready':     ex.args[0]['status'] == "COMPLETE",
+      'ready':     ex.args[0]['status'] == TaskStatus.COMPLETE,
       'data_hash': ex.args[0].data_hash,
       'id':        ex.args[0].id,
     }

src/modules/site-v2/base/views/tools/genetic_mapping.py
@@ -19,6 +19,7 @@ from caendr.models.error import (
     FileUploadError,
     ReportLookupError,
 )
+from caendr.models.task import TaskStatus
 
 
 
@@ -126,6 +127,7 @@ def all_reports():
     'subtitle': 'Report List',
     'alt_parent_breadcrumb': {"title": "Tools", "url": url_for('tools.tools')},
     'mappings': get_all_mappings(),
+    'TaskStatus': TaskStatus,
   })
 
 
@@ -138,6 +140,7 @@ def user_reports():
     'subtitle': 'Report List',
     'alt_parent_breadcrumb': {"title": "Tools", "url": url_for('tools.tools')},
     'mappings': get_user_mappings(user.name),
+    'TaskStatus': TaskStatus,
   })
 
 

src/modules/site-v2/base/views/tools/heritability_calculator.py
@@ -30,6 +30,7 @@ from caendr.models.error import (
     UnfinishedReportError,
 )
 from caendr.models.datastore import SPECIES_LIST, HeritabilityReport
+from caendr.models.task import TaskStatus
 from caendr.api.strain import get_strains
 from caendr.services.heritability_report import get_all_heritability_results, get_user_heritability_results, create_new_heritability_report, get_heritability_report, fetch_heritability_report
 from caendr.utils.data import unique_id, convert_data_table_to_tsv, get_object_hash
@@ -84,6 +85,7 @@ def all_results():
     'title': "All Heritability Results",
     'alt_parent_breadcrumb': {"title": "Tools", "url": url_for('tools.tools')},
     'items': get_all_heritability_results(),
+    'TaskStatus': TaskStatus,
   })
 
 
@@ -95,6 +97,7 @@ def user_results():
     'title': "My Heritability Results",
     'alt_parent_breadcrumb': {"title": "Tools", "url": url_for('tools.tools')},
     'items': get_user_heritability_results(user.name),
+    'TaskStatus': TaskStatus,
   })
 
 
@@ -239,7 +242,7 @@ def report(id):
   # If result exists, mark as complete
   # TODO: Is this the right place for this?
   if result:
-    hr.status = 'COMPLETE'
+    hr.status = TaskStatus.COMPLETE
     hr.save()
 
   # TODO: Are either of these values used?

src/modules/site-v2/templates/tools/genetic_mapping/list-all.html
@@ -60,7 +60,7 @@ table.dataTable thead .sorting {
               <td class='data-label'> {{ item.label }} </td>
               <td class='data-trait'> {{ item.trait }} </td>
               <td class='data-status'>
-                {% if item.status == 'COMPLETE' %}
+                {% if item.status == TaskStatus.COMPLETE %}
                 <a href="{{ url_for('genetic_mapping.report', id=item.id) }}">
                   {{ item.status }}
                 </a>

src/modules/site-v2/templates/tools/genetic_mapping/list-user.html
@@ -59,7 +59,7 @@ table.dataTable thead .sorting {
               <td class='data-label'> {{ item.label }} </td>
               <td class='data-trait'> {{ item.trait }} </td>
               <td class='data-status'>
-                {% if item.status == 'COMPLETE' %}
+                {% if item.status == TaskStatus.COMPLETE %}
                 <a href="{{ url_for('genetic_mapping.report', id=item.id) }}">
                   {{ item.status }}
                 </a>

src/modules/site-v2/templates/tools/heritability_calculator/result.html
@@ -12,7 +12,7 @@
 <script type="text/javascript"
   src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.2/FileSaver.min.js"></script>
 
-{% if not (result or hr.status in ['ERROR','COMPLETE']) %}
+{% if not (result or hr.status in [TaskStatus.ERROR, TaskStatus.COMPLETE]) %}
 <meta http-equiv="refresh" content="10; url={{ request.path }}">
 {% endif %}
 
@@ -38,9 +38,9 @@
       <strong>Created:</strong> {{ hr.created_on | date_format }}
    </p>
    <p>
-      <strong>Status:</strong>  {{ hr.status}}
+      <strong>Status:</strong>  {{ hr.status }}
     
-    {% if hr.status == 'ERROR' %}
+    {% if hr.status == TaskStatus.ERROR %}
       <strong>Error:</strong> <span class="text-danger">{{ operation.error }}</span>
     {% endif %}
    </p>
@@ -67,7 +67,7 @@
       <!-- Heritability Results -->
       <div class="col">
         {% if data and not result %}
-        {% if not hr.status in ['COMPLETE','ERROR'] %}
+        {% if not hr.status in [TaskStatus.COMPLETE, TaskStatus.ERROR] %}
         <p class='text-info text-center'>
           <strong>
             The heritability calculation is currently being run. Please check back in a few minutes for results.

src/modules/site-v2/templates/tools/pairwise_indel_finder/list-all.html
@@ -61,7 +61,7 @@ table.dataTable thead .sorting {
               <td class="data-empty" data-order='0'></td>
             {% endif %}
               <td class="data-status">
-                {% if item.status == 'COMPLETE' %}
+                {% if item.status == TaskStatus.COMPLETE %}
                 <a href="{{ url_for('pairwise_indel_finder.report', id=item.id) }}">
                   {{ item.status }}
                 </a>

commit 203a6787da5cd9415ebba5a6be2c90a01449a690

Bugfix: add TaskStatus when rendering Indel Finder results list
Fixes bug introduced in f918dc969716c96e4ee2b86fa04ebe21763e70dc

src/modules/site-v2/base/views/tools/pairwise_indel_finder.py
@@ -124,6 +124,8 @@ def all_results():
     # User info
     'user':  get_current_user(),
     'items': get_all_indel_primers(),
+
+    'TaskStatus': TaskStatus,
   })
 
 
@@ -144,6 +146,8 @@ def user_results():
     # User info
     'user':  user,
     'items': get_user_indel_primers(user.name),
+
+    'TaskStatus': TaskStatus,
   })
 
 