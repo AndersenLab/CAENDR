67ec4abc1daf9da90a4f016e7b789a45eeeaee13
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2023-04-18 13:26:50 -0500

    Bugfix: Indel Finder submission AJAX request
    Distinguish between submissions that deliver the data using FormData (Nemascan, Heritability) vs delivering the data using stringified JSON (Indel Primer). Affects headers of unified AJAX call.

--- src/modules/site-v2/templates/_scripts/submit-job.js
+++ src/modules/site-v2/templates/_scripts/submit-job.js
@@ -1,15 +1,31 @@
 {#
-  Tool name must have endpoints '.submit' and '.report' to work properly.
+  Must be used before AJAX requests can run.
 #}
-{% macro def_submit_job(tool_name, func_name='submit_job') %}
+{% macro ajax_setup(token) %}
+$.ajaxSetup({
+  beforeSend: function(xhr, settings) {
+    if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+      xhr.setRequestHeader("X-CSRFToken", "{{ token }}")
+    }
+  }
+});
+{% endmacro %}
+
+
+{#
+  Tool name must have endpoints '.submit' and '.report' to work properly.
+
+  If defining with `as_form_data` = True, must include `ajax_setup` on the page (defined above).
+#}
+{% macro def_submit_job(tool_name, as_form_data=false, func_name='submit_job') %}
 function {{func_name}}(data, modal_id, new_tab=false) {
 
   return $.ajax({
     type: "POST",
-    processData: false,
-    contentType: false,
+    {% if as_form_data -%} processData: false, {% endif %}
+    contentType: {% if as_form_data -%} false {%- else -%} "application/json; charset=utf-8" {%- endif -%},
     dataType: 'json',
-    data: data,
+    data: {% if as_form_data -%} data {%- else -%} JSON.stringify(data) {%- endif -%},
     url:
       {%- if session["is_admin"] %}
         "{{ url_for(tool_name + '.submit') }}" + ($('#no_cache_checkbox').prop('checked') ? "?nocache=1" : ""),
