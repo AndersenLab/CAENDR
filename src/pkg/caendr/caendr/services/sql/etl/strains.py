0ed011a459eadb577dc232ba46be53a536f940b5
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2023-05-10 10:46:57 -0500

    Add tropicalis strain sheet secret to list

--- src/pkg/caendr/caendr/services/sql/etl/strains.py
+++ src/pkg/caendr/caendr/services/sql/etl/strains.py
@@ -7,15 +7,15 @@ from caendr.services.cloud.secret import get_secret
 from caendr.models.sql import Strain
 
 ANDERSEN_LAB_STRAIN_SHEETS = [
-  get_secret('ANDERSEN_LAB_STRAIN_SHEET'),            # elegans
-  get_secret('ANDERSEN_LAB_STRAIN_SHEET_C_BRIGGSAE'), # briggsae
-  # tropicalis
+  get_secret('ANDERSEN_LAB_STRAIN_SHEET'),              # elegans
+  get_secret('ANDERSEN_LAB_STRAIN_SHEET_C_BRIGGSAE'),   # briggsae
+  get_secret('ANDERSEN_LAB_STRAIN_SHEET_C_TROPICALIS'), # tropicalis
 ]
 
 elevation_cache = {}
 NULL_VALS = ["None", "", "NA", None]
 
-def load_strains(db): 
+def load_strains(db):
   logger.info('Loading strains...')
   for sheet_id in ANDERSEN_LAB_STRAIN_SHEETS:
     andersen_strains = fetch_andersen_strains(sheet_id)
