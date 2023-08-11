2f758b032c1d57c4c583d9c0e16345f674ad9ccd
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2023-08-11 10:41:40 -0500

    Manage local temp files w new LocalFile class
    Use as context manager to temporarily upload an input file to the server, and delete file when context ends.
    Gathers upload & clean up logic into one place.
    
    Should be general enough to use for any upload files, not just tool inputs.

--- src/modules/site-v2/base/utils/tools.py
+++ src/modules/site-v2/base/utils/tools.py
@@ -3,7 +3,6 @@ import os
 from caendr.services.logger import logger
 
 from base.utils.auth import get_current_user, user_is_admin
-from werkzeug.utils import secure_filename
 
 from caendr.models.datastore import IndelPrimer, NemascanMapping, HeritabilityReport
 from constants import TOOL_INPUT_DATA_VALID_FILE_EXTENSIONS
@@ -12,16 +11,12 @@ from caendr.models.error import (
     CachedDataError,
     DataFormatError,
     DuplicateDataError,
-    FileUploadError,
     ReportLookupError,
 )
 from caendr.services.tools import submit_job
-from caendr.utils.data import unique_id
 from caendr.services.cloud.secret import get_secret
 
 
-uploads_dir = os.path.join('./', 'uploads')
-os.makedirs(uploads_dir, exist_ok=True)
 
 SUPPORT_EMAIL = get_secret('SUPPORT_EMAIL')
 
@@ -30,6 +25,14 @@ def get_class_from_kind(kind):
   for c in [ IndelPrimer, NemascanMapping, HeritabilityReport ]:
     if kind == c.kind:
       return c
+
+
+def get_upload_err_msg(code):
+  CODE_TO_MSG = {
+    400: 'You must include a CSV file with your data to upload. If this message persists, try refreshing the page and re-uploading your file.',
+    500: f'There was a problem uploading your submission. Please try again. If this problem persists, please contact us at {SUPPORT_EMAIL}',
+  }
+  return CODE_TO_MSG.get(code, CODE_TO_MSG[500])
 
 
 def lookup_report(kind, reportId, user=None, validate_user=True):
@@ -66,49 +69,6 @@ def lookup_report(kind, reportId, user=None, validate_user=True):
 
   # If all checks passed, return the report entity
   return report
-
-
-def upload_file(request, filename, valid_file_extensions=None):
-  '''
-    Save uploaded file to server temporarily with unique generated name.
-    Copies the file extension of the provided file
-
-    Arguments:
-      request: The Flask request object.
-      filename (str): The name of the uploaded file in the request.
-      valid_file_extensions(list(str), optional):
-        A list of allowed file extensions. If filename does not have a valid extension, raises an error. If not provided, accepts any file extension.
-  '''
-
-  # Get the FileStorage object from the request
-  file = request.files.get(filename)
-  if not file:
-    raise FileUploadError('You must include a CSV file with your data to upload. If this message persists, try refreshing the page and re-uploading your file.', 400)
-
-  # Match the file extension by splitting on right-most '.' character
-  try:
-    file_ext = file.filename.rsplit('.', 1)[1]
-  except:
-    file_ext = None
-
-  # Validate file extension, if file_ext and valid_file_extensions are defined
-  if file_ext and valid_file_extensions and (file_ext not in valid_file_extensions):
-    raise FileUploadError('You must include a CSV file with your data to upload. If this message persists, try refreshing the page and re-uploading your file.', 400)
-
-  # Create a unique local filename for the file
-  # TODO: Is there a better way to generate this name?
-  #       Using a Tempfile, using the user's ID and the time, etc.?
-  target_filename = unique_id() + (f'.{file_ext}' if file_ext else '')
-  local_path = os.path.join(uploads_dir, secure_filename(target_filename))
-
-  # Save the file, alerting the user if this fails
-  try:
-    file.save(local_path)
-  except Exception:
-    raise FileUploadError(f'There was a problem uploading your submission. Please try again. If this problem persists, please contact us at {SUPPORT_EMAIL}', 500)
-
-  # Return the name of the file on the server
-  return local_path
 
 
 def try_submit(EntityClass, user, data, no_cache):
