59944bb8e91486a7899dfd3f2972bae31ae46117
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2023-10-03 14:38:32 -0500

    Indentation

--- src/pkg/caendr/caendr/models/datastore/__init__.py
+++ src/pkg/caendr/caendr/models/datastore/__init__.py
@@ -1,20 +1,20 @@
-from .entity import Entity
-from .container import Container
-from .user import User
-from .job_entity import JobEntity
-from .user_owned_entity import UserOwnedEntity
-from .data_job_entity import DataJobEntity
-from .dataset_release import DatasetRelease
-from .profile import Profile
-from .nemascan_mapping import NemascanMapping
-from .pipeline_operation import PipelineOperation
-from .database_operation import DatabaseOperation
-from .indel_primer import IndelPrimer
+from .entity              import Entity
+from .container           import Container
+from .user                import User
+from .job_entity          import JobEntity
+from .user_owned_entity   import UserOwnedEntity
+from .data_job_entity     import DataJobEntity
+from .dataset_release     import DatasetRelease
+from .profile             import Profile
+from .nemascan_mapping    import NemascanMapping
+from .pipeline_operation  import PipelineOperation
+from .database_operation  import DatabaseOperation
+from .indel_primer        import IndelPrimer
 from .heritability_report import HeritabilityReport
 from .gene_browser_tracks import GeneBrowserTracks
-from .markdown import Markdown
-from .wormbase import WormbaseVersion, WormbaseProjectNumber
-from .species import Species, SPECIES_LIST
+from .markdown            import Markdown
+from .wormbase            import WormbaseVersion, WormbaseProjectNumber
+from .species             import Species, SPECIES_LIST
 
 
 def get_entity_by_kind(kind, name):
