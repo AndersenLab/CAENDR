89d10689a283d0a2c9839fc3842fbffa167140df
Orzu Tursunova <orzut@github.com>
2023-10-13 13:16:32 -0500

    deleted unused code, added comments

--- src/pkg/caendr/caendr/models/datastore/cart.py
+++ src/pkg/caendr/caendr/models/datastore/cart.py
@@ -42,12 +42,14 @@ class Cart(DeletableEntity):
     
     # Try finding cart(s) for user
     try:
+      # Run the query and try to find not deleted cart(s) for the user
       carts = cls.query_ds_not_deleted('user', email, required=True)
+
       if len(carts) == 1:
         # If only one cart was found, return it
         return carts[0]
       else:
-        # If multiple carts found for user, sort by date modified on and return latest
+        # If multiple carts were found for user, sort by date modified on and return latest
         return cls.sort_by_modified_date(carts, reverse=True)[0]
 
     # If a user doesn't have a cart, create one
@@ -85,98 +87,3 @@ class Cart(DeletableEntity):
 
   def update_version(self):
     self['version'] += 1
-
-  # def soft_delete(self):
-  #   self['is_deleted'] = True
-
-
-
-
-
-
-
-# class Cart(Entity):
-#   kind = 'cart'
-
-#   def __init__(self, name_or_obj = None, *args, **kwargs): # If nothing passed for name_or_obj, create a new ID to use for this object
-#     if name_or_obj is None: 
-#       name_or_obj = unique_id()
-#       self.set_properties_meta(id = name_or_obj) 
-
-#   # Initialize from superclass 
-#     super().__init__(name_or_obj, *args, **kwargs)
-#     if not self['is_deleted']:
-#       self['is_deleted'] = False
-#     if not self['items']:
-#       self['items'] = []
-#     if not self['version']:
-#       self['version'] = 0
-  
-#   ## Properties List ##
-
-#   @classmethod
-#   def get_props_set(cls):
-#     return {
-#       *super().get_props_set(),
-#       'user',
-#       'items',
-#       'is_deleted',
-#       'version'
-#     }
-  
-  
-
-#   @classmethod
-#   def lookup_by_user(cls, email):
-
-#     # Try finding and returning unique cart for this user
-#     try:
-#       return Cart.query_ds_not_deleted('user', email, required=True)
-
-
-#     # If a user doesn't have a cart, create one
-#     except NotFoundError:
-#       return Cart(**{'user': email, 'items': [], 'version': 0})
-
-#     # Multiple carts found for user
-#     # TODO: get all carts that match the user, sort by date created on and return the latest one?
-#     except NonUniqueEntity as ex:
-#       return Cart.sort_by_modified_date(ex.matches, reverse=True)[0]
-
-
-#   @classmethod
-#   def get_price(self, item):
-#     if item['name'] == 'Flat Rate Shipping':
-#       return PRICES.SHIPPING
-#     elif item['name'] == "set_divergent":
-#       return PRICES.DIVERGENT_SET
-#     elif item['name'].startswith("set"):
-#       return PRICES.STRAIN_SET
-#     else:
-#       return PRICES.STRAIN
-
-
-#   def transfer_to_user(self, email):
-#     self['user'] = email
-
-
-#   def add_item(self, item):
-#     for cartItem in self['items']:
-#       if cartItem['name'] == item['name']:
-#         return
-#     self['items'].append(item)
-
-
-#   def remove_item(self, item):
-#     for cartItem in self['items']:
-#       if cartItem['name'] == item:
-#         self['items'].remove(cartItem)
-
-
-#   def soft_delete(self):
-#     self['is_deleted'] = True
-
-#   def update_version(self):
-#     self['version'] += 1
-
-
--- src/pkg/caendr/caendr/models/datastore/deletable_entity.py
+++ src/pkg/caendr/caendr/models/datastore/deletable_entity.py
@@ -5,22 +5,17 @@ from caendr.models.error import NotFoundError, NonUniqueEntity
 
 class DeletableEntity(Entity):
   '''
-  Subclass of Entity for entities that need 'soft delete' functionality
+    Subclass of Entity for objects that need 'soft delete' functionality
+
+    Should never be instantiated directly -- in fact, this is prevented in this class's __new__ function.
+    Instead, specific entity types should be subclasses of this class.
   '''
-  # TODO: Documentation string (above) that explains what this class is for
-
-
-  # TODO: __new__ function that prevents this subclass from being instantiated directly
-  #       Hint: Take a look at UserOwnedEntity
   
   def __new__(cls, *args, **kwargs):
     if cls is DeletableEntity:
       raise TypeError(f"Class '{cls.__name__}' should never be instantiated directly -- subclasses should be used instead.")
     return super(DeletableEntity, cls).__new__(cls)
 
-  # TODO: __init__ function
-  #       Should use super() to call the __init__ method of Entity,
-  #       and initialize any properties unique to this subclass (from get_props_set)
 
   def __init__(self, *args, **kwargs):
   # Initialize from superclass 
@@ -33,16 +28,23 @@ class DeletableEntity(Entity):
   def get_props_set(cls):
     return {
       *super().get_props_set(),
-      # TODO: List any properties that belong in this subclass
       'is_deleted'
     }
   
 
-  # TODO: Function to query only entities that haven't been deleted
-  #       Hint: Take a look at Cart
 
   @classmethod
   def query_ds_not_deleted(cls, key, val, unique=False, required=False):
+
+    '''
+      Query the datatstore for the entities that are 'not_deleted'.
+
+      If 'unique' is set to True and multiple entities were found will raise NonUniqueEntity error with all matches in the `matches` field;
+      otherwise return all entities that match the query
+
+      If `required` is set to True, will raise an error if no such entity is found; otherwise returns None.
+
+    '''
 
     # Run query with given key and val
     matches = cls.query_ds(filters=[(key, '=', val)])
@@ -55,22 +57,20 @@ class DeletableEntity(Entity):
       else:
         return None
 
-    # If exactly one entity found, return it
+    # If looking up for unique entity and multiple were found, raise an error
     elif len(matches) > 1 and unique:
       raise NonUniqueEntity( cls.kind, key, val, matches )
     
-    # If more than one entity found, return all
+    # If one or more entities were found and 'unique' is set to False, return all of them
     else:
       return matches
 
 
-  # TODO: Function to soft delete this object
-  #       Hint: Take a look at Cart
   def soft_delete(self):
+    ''' Flag the entity deleted '''
     self['is_deleted'] = True
 
-  # TODO: Function to un-delete (restore?) this object?
-  #       Might not be useful now, but it's a good idea to have one
   def undelete(self):
+    ''' Flag the entity un-deleted '''
     self['is_deleted'] = False
 
