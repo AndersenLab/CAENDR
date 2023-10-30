c09770111b399254524319b6b7811c1c592d4925
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2023-10-30 17:53:48 -0500

    Fix: remove job-specific Runner imports

--- src/pkg/caendr/caendr/models/run/__init__.py
+++ src/pkg/caendr/caendr/models/run/__init__.py
@@ -2,6 +2,3 @@
 from .base   import Runner
 from .gcp    import GCPRunner, GCPCloudRunRunner, GCPLifesciencesRunner
 from .local  import LocalRunner
-
-# Job-specific subclasses
-from .tools_gcp_cloudrun import DatabaseOperationRunner, IndelPrimerRunner, HeritabilityRunner, NemascanRunner
