2bfec2e4ff9128e011df89e574c862e68ad0992b
Orzu Tursunova <orzut@github.com>
2023-11-15 09:35:30 -0600

    Created parse function for traits files

--- src/pkg/caendr/caendr/services/sql/etl/phenotype_db.py
+++ src/pkg/caendr/caendr/services/sql/etl/phenotype_db.py
@@ -8,41 +8,39 @@ from sqlalchemy.sql.expression import null
 
 
 
-# TODO: Write a parsing function for the phenotype file(s)
-#       You should get the list of filenames as a list of strings
-#       (Don't worry about start_idx)
-
-def parse_strain_variant_annotation_data(species, *fnames: str, start_idx = 0):
+def parse_phenotypedb_traits_data(species, *fnames: str):
   logger.info('Parsing extracted phenotype database TSV file(s)')
 
   # Loop through each line in each TSV file, indexed
   for fname in fnames:
-    with gzip.open(fname, mode='rt') as csv_file:
+    with open(fname) as csv_file:
       for idx, row in enumerate( csv.reader(csv_file, delimiter='\t') ):
 
         # First line is column names - don't interpret as data
-        # TODO: Create some data structure that lets you map a column index to a strain name
-        #       Hint -- I think you could just save the strains as a list, and zip each data row with that list
         if idx == 0:
           logger.info(f'Column names in file "{fname}" are: {", ".join(row)}')
-
-          # column_header_map = { name: idx for idx, name in enumerate(row) }
+          column_header_map = { name: idx for idx, name in enumerate(row) }
           continue
-
-        # If testing, finish early
-        if os.getenv("USE_MOCK_DATA") and idx > 10:
-          logger.warn("USE_MOCK_DATA Early Return!!!")
-          return
 
         # Progress update
         if idx % 1000000 == 0:
           logger.debug(f"Processed {idx} lines")
 
-        # TODO: Any processing required for this line
+        # Get trait value for each strain
+        trait_name = row[0]
+        for header in column_header_map:
+          if column_header_map[header] == 0:
+            continue
+          strain_name = header
+          trait_value = row[column_header_map[header]]
 
-        # TODO: Yield a *single* row as a dict
-        #       This might have to go in a loop, to yield each trait measurement as a new row?
-        yield {}
+        # Yield each trait measurement as a new row
+          yield {
+            'trait_name':   trait_name,
+            'strain':       strain_name,
+            'trait_value':  trait_value
+          }
+
 
   # In Python, loop vars maintain their final value after the loop ends
   print(f'Processed {idx} lines total for {species.name}')
