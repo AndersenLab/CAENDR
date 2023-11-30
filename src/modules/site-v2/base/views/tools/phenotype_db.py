84b9fc053b4418c9ba63bf9411eb3f7da8bc62c8
Orzu Tursunova <orzut@github.com>
2023-11-30 15:34:00 -0600

    Phenotype db template skeleton

--- src/modules/site-v2/base/views/tools/phenotype_db.py
+++ src/modules/site-v2/base/views/tools/phenotype_db.py
@@ -10,4 +10,9 @@ phenotype_db_bp = Blueprint(
   'phenotype_db', __name__, template_folder='templates'
 )
 
-
+@phenotype_db_bp.route('/table')
+def get_table():
+    return render_template('tools/phenotype_db/table.html', **{
+      "title": 'Phenotype Database',
+      "tool_alt_parent_breadcrumb": { "title": "Tools", "url": url_for('tools.tools') },
+    })
