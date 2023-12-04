70ce02eceb033faf75d15937c3e733d123faa0c6
Orzu Tursunova <orzut@github.com>
2023-12-04 14:22:52 -0600

    Fetching traits from datastore into view function

--- src/modules/site-v2/base/views/tools/phenotype_db.py
+++ src/modules/site-v2/base/views/tools/phenotype_db.py
@@ -5,6 +5,9 @@ from flask import ( render_template,
                     abort,
                   )
 
+from caendr.services.logger import logger
+from caendr.models.datastore import TraitFile
+
 
 phenotype_db_bp = Blueprint(
   'phenotype_db', __name__, template_folder='templates'
@@ -12,7 +15,24 @@ phenotype_db_bp = Blueprint(
 
 @phenotype_db_bp.route('/table')
 def get_table():
+    """ Phenotype Database Traits Table """
+
+    try:
+      trait_files_list = TraitFile.query_ds()
+    except Exception as ex:
+       logger.error(f'Failed to retireve traits list: {ex}')
+       abort(500)
+
     return render_template('tools/phenotype_db/table.html', **{
-      "title": 'Phenotype Database',
+        
+      # Page Info
+      "title": 'Phenotype Database and Analysis',
       "tool_alt_parent_breadcrumb": { "title": "Tools", "url": url_for('tools.tools') },
+
+      # Data
+      "traits_listing": trait_files_list
     })
+
+@phenotype_db_bp.route('/select')
+def analysis_selection():
+    pass
