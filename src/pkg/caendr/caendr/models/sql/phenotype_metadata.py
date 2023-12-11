5baa6efd61bedc1118cad1d76fbb3da29e4b2bef
Orzu Tursunova <orzut@github.com>
2023-12-11 13:56:28 -0600

    Added Phenotype Metadata SQL table

---
+++ src/pkg/caendr/caendr/models/sql/phenotype_metadata.py
@@ -0,0 +1,21 @@
+from caendr.services.cloud.postgresql import db
+from caendr.models.sql.dict_serializable import DictSerializable
+
+class PhenotypeMetadata(DictSerializable, db.Model):
+  """
+      Phenotype Metadata table serves as a repository for metadata associated with various traits. 
+      It complements a system where trait-related information is stored PhenotypeDatabase table. 
+      This table includes details such as species, description, source lab, and other additional 
+      information.
+  """
+  trait_name = db.Column(db.String(), primary_key=True)
+  species = db.Column(db.String(20))
+  description_short = db.Column(db.String(), nullable=True)
+  description_long = db.Column(db.Text(), nullable=True)
+  units = db.Column(db.String(), nullable=True)
+  doi = db.Column(db.String(), nullable=True)
+  protocols = db.Column(db.String(), nullable=True)
+  source_lab = db.Column(db.String())
+  created_on = db.Column(db.Date())
+
+  __tablename__ = 'phenotype_metadata'
