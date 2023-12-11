9f8a4ac2287e78d22d99fd798eab535df31255a6
Orzu Tursunova <orzut@github.com>
2023-12-11 12:24:37 -0600

    Phenotype DB SQL table description

--- src/pkg/caendr/caendr/models/sql/phenotype.py
+++ src/pkg/caendr/caendr/models/sql/phenotype.py
@@ -3,7 +3,9 @@ from caendr.models.sql.dict_serializable import DictSerializable
 
 class PhenotypeDatabase(DictSerializable, db.Model):
   """
-      Table description
+      Phenotype database table captures data related to various traits exhibited 
+      by different strains. Each row represents a specific combination of strain, 
+      trait, and the corresponding trait value
   """
   trait_name = db.Column(db.String(), primary_key=True)
   strain_name = db.Column(db.String(), primary_key=True)
