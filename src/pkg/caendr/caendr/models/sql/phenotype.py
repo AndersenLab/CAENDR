05f5825005e41975d0ca300aa438dde8449f4546
Orzu Tursunova <orzut@github.com>
2023-12-18 12:57:47 -0600

    Updating SQL tables to new fields

--- src/pkg/caendr/caendr/models/sql/phenotype.py
+++ src/pkg/caendr/caendr/models/sql/phenotype.py
@@ -7,7 +7,7 @@ class PhenotypeDatabase(DictSerializable, db.Model):
       by different strains. Each row represents a specific combination of strain, 
       trait, and the corresponding trait value
   """
-  trait_name = db.Column(db.String(), db.ForeignKey('phenotype_metadata.trait_name'), primary_key=True, nullable=False)
+  trait_name = db.Column(db.String(), db.ForeignKey('phenotype_metadata.trait_name_caendr'), primary_key=True)
   strain_name = db.Column(db.String(), primary_key=True)
   trait_value = db.Column(db.Float())
 
--- src/pkg/caendr/caendr/models/sql/phenotype_metadata.py
+++ src/pkg/caendr/caendr/models/sql/phenotype_metadata.py
@@ -8,21 +8,29 @@ class PhenotypeMetadata(DictSerializable, db.Model):
       This table includes details such as species, description, source lab, and other additional 
       information.
   """
-  trait_name = db.Column(db.String(), unique=True, primary_key=True)
-  species = db.Column(db.String(20))
+
+  trait_name_caendr = db.Column(db.String(), unique=True, primary_key=True)
+  trait_name_user = db.Column(db.String(), nullable=True)
+  species = db.Column(db.String(20)),
+  wbgene_id = db.Column(db.String(20), nullable=True),
   description_short = db.Column(db.String(), nullable=True)
   description_long = db.Column(db.Text(), nullable=True)
   units = db.Column(db.String(), nullable=True)
-  doi = db.Column(db.String(), nullable=True)
+  publication = db.Column(db.String(), nullable=True)
   protocols = db.Column(db.String(), nullable=True)
   source_lab = db.Column(db.String())
+  institution = db.Column(db.String())
+  tags = db.Column(db.String(), nullable=True)
+  capture_date = db.Column(db.Date(), nullable=True)
   created_on = db.Column(db.Date(), nullable=False)
+  modified_on = db.Column(db.Date())
+  dataset = db.Column(db.String(), nullable=True)
   is_bulk_file = db.Column(db.Boolean(), nullable=False)
   phenotype_values = db.relationship(
                       'PhenotypeDatabase', 
                       backref='phenotype_db.trait_name', 
-                      primaryjoin='PhenotypeMetadata.trait_name==PhenotypeDatabase.trait_name', 
-                      lazy='joined')
+                      primaryjoin='PhenotypeMetadata.trait_name_caendr==PhenotypeDatabase.trait_name', 
+                      lazy='select')
 
   __tablename__ = 'phenotype_metadata'
 
