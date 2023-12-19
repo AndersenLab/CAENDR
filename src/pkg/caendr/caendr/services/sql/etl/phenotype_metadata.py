6dff197494274095938bbf64cef3d2accce75ea2
Orzu Tursunova <orzut@github.com>
2023-12-19 11:46:54 -0600

    Updating the attributes access for metadata parsing function

--- src/pkg/caendr/caendr/services/sql/etl/phenotype_metadata.py
+++ src/pkg/caendr/caendr/services/sql/etl/phenotype_metadata.py
@@ -14,14 +14,14 @@ def parse_phenotype_metadata(species: Species, **files: LocalDatastoreFile):
   
   logger.info('Parsing function for traits metadata')
   
-  for fname, fpath in files.items():
+  for file_name, file_object in files.items():
 
     # Get metadata for the file 
-    md = fpath._metadata
+    md = file_object.metadata
 
     # If bulk file, open it and yield each trait as a new row with the file's metadata
     if md.is_bulk_file:
-      with open(fpath) as csv_file:
+      with open(file_object) as csv_file:
         for idx, row in enumerate( csv.reader(csv_file, delimiter='\t') ):
           if idx == 0:
             continue
@@ -39,7 +39,7 @@ def parse_phenotype_metadata(species: Species, **files: LocalDatastoreFile):
              'protocols':         md['protocols'],
              'source_lab':        md['source_lab'],
              'institution':       md['institution'],
-             'submitted_by':      md.serialize()['submitter'],
+             'submitted_by':      md.get_user().full_name,
              'tags':              md['tags'],
              'capture_date':      md['capture_date'],
              'created_on':        md.created_on,
@@ -60,7 +60,7 @@ def parse_phenotype_metadata(species: Species, **files: LocalDatastoreFile):
       'protocols':         md['protocols'],
       'source_lab':        md['source_lab'],
       'institution':       md['institution'],
-      'submitted_by':      md.serialize()['submitter'],
+      'submitted_by':      md.get_user().full_name,
       'tags':              md['tags'],
       'capture_date':      md['capture_date'],
       'created_on':        md.created_on,
