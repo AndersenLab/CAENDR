e6977d7ef392b1faefe348cebee1f0f2f16a0751
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2024-02-28 10:45:18 -0600

    Move trait JSON query to util function

--- src/modules/site-v2/templates/_scripts/trait_utils.js
+++ src/modules/site-v2/templates/_scripts/trait_utils.js
@@ -11,3 +11,20 @@ function traitNameHTML(name_1, name_2, name_3) {
   }
   return nameHTML;
 }
+
+
+function queryTraitByName(trait_name, csrf_token=null) {
+
+  // Construct the data object, using the optional CSRF token if provided
+  let data = {trait_name};
+  if (csrf_token !== null) data['csrf_token'] = csrf_token;
+
+  // Return the AJAX request as a Promise object
+  return $.ajax({
+      type:        "POST",
+      url:         "{{ url_for('phenotype_database.get_traits_json') }}",
+      data:        JSON.stringify({trait_name}),
+      contentType: "application/json",
+      dataType:    "json",
+  });
+}
