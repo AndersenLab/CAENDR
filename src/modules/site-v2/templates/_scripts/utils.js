e34ba69b4194243dbd479abe440de8d0ec22df65
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2024-04-08 12:34:03 -0500

    Modularize trait offcanvas sidebar JS code

--- src/modules/site-v2/templates/_scripts/utils.js
+++ src/modules/site-v2/templates/_scripts/utils.js
@@ -151,3 +151,14 @@ function flash_message(message, full_msg_link=null, full_msg_body=null) {
   {%- endfor %}
   {%- endif %}
 }
+
+
+/* Format a date as YYYY-MM-DD.
+ */
+function formatDate(d) {
+  if(d === null) return 'N/A'
+  const date = new Date(d)
+  const month = (date.getMonth()+1).toString().padStart(2, '0')
+  const day = date.getDate().toString().padStart(2, '0')
+  return `${date.getFullYear()}-${month}-${day}`
+}
