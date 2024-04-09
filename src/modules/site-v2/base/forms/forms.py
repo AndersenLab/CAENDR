86a07b2d8b5b5c15db1b2dc93f315d2a1d6b1721
Orzu Tursunova <orzut@github.com>
2024-04-09 11:00:40 -0500

    Update form validators

--- src/modules/site-v2/base/forms/forms.py
+++ src/modules/site-v2/base/forms/forms.py
@@ -344,14 +344,14 @@ class TraitSubmissionForm(FlaskForm):
   species_name = SpeciesSelectField(validators=[Required()])
   trait_name_user = StringField('Trait Name:', validators=[Required(), Length(min=3, max=50)])
   description_short = TextAreaField('Short Description:', validators=[Required(), Length(min=10, max=200)])
-  description_long = TextAreaField('Long Description:', validators=[Length(min=10)])
-  units = StringField('Unit of Measurement:', validators=[Length(min=1, max=10)])
+  description_long = TextAreaField('Long Description:')
+  units = StringField('Unit of Measurement:', validators=[Length(min=0, max=10)])
   tags = MultiCheckboxField("Categories:", choices=TRAIT_CATEGORY_OPTIONS, validators=[Required()])
   submitted_by = StringField('Username:', validators=[Required(), Length(min=3, max=50)])
   institution = StringField('Institution:', validators=[Required(), Length(min=3, max=50)])
   source_lab = StringField('Source Lab:', validators=[Required(), Length(min=1, max=4)])
-  protocols = TextAreaField('Protocols:', validators=[Length(min=1, max=200)])
-  publication = TextAreaField('Publications:', validators=[Length(min=1, max=200)])
+  protocols = TextAreaField('Protocols:', validators=[Length(min=0, max=200)])
+  publication = TextAreaField('Publications:', validators=[Length(min=0, max=200)])
 
 
 
