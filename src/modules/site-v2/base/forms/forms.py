a5e9fa27ccad1d1b35d64cbfe5795d868ea760ba
Orzu Tursunova <orzut@github.com>
2024-04-12 10:16:34 -0500

    Render tooltips with macro

--- src/modules/site-v2/base/forms/forms.py
+++ src/modules/site-v2/base/forms/forms.py
@@ -340,21 +340,21 @@ class StrainListForm(Form):
 
 class TraitSubmissionForm(FlaskForm):
   """ The trait submission form """
-  file = FileField('Select file:', render_kw={'accept': ','.join({ f'.{ext}' for ext in TOOL_INPUT_DATA_VALID_FILE_EXTENSIONS})}, validators=[Required()])
+  file = FileField('Select file', render_kw={'accept': ','.join({ f'.{ext}' for ext in TOOL_INPUT_DATA_VALID_FILE_EXTENSIONS})}, validators=[Required()])
   species = SpeciesSelectField(validators=[Required()])
-  trait_name_user = StringField('Trait Name:', validators=[Required(), Length(min=3, max=50)])
-  trait_name_display_1 = StringField('Display Name 1:', validators=[Required(), Length(min=3, max=50)])
-  trait_name_display_2 = StringField('Display Name 2:')
-  trait_name_display_3 = StringField('Display Name 3:')
-  description_short = TextAreaField('Short Description:', validators=[Required(), Length(min=10, max=200)])
-  description_long = TextAreaField('Long Description:')
-  units = StringField('Unit of Measurement:', validators=[Length(min=0, max=10)])
-  tags = MultiCheckboxField("Categories:", choices=TRAIT_CATEGORY_OPTIONS, validators=[Required()])
-  username = StringField('Username:', validators=[Required(), Length(min=3, max=50)])
-  institution = StringField('Institution:', validators=[Required(), Length(min=3, max=50)])
-  source_lab = StringField('Source Lab:', validators=[Required(), Length(min=1, max=4)])
-  protocols = TextAreaField('Protocols:', validators=[Length(min=0, max=200)])
-  publication = TextAreaField('Publications:', validators=[Length(min=0, max=200)])
+  trait_name_user = StringField('Internal Trait Name', validators=[Required(), Length(min=3, max=50)])
+  trait_name_display_1 = StringField('Display Name 1', validators=[Required(), Length(min=3, max=50)])
+  trait_name_display_2 = StringField('Display Name 2')
+  trait_name_display_3 = StringField('Display Name 3')
+  description_short = TextAreaField('Short Description', validators=[Required(), Length(min=10, max=200)])
+  description_long = TextAreaField('Long Description')
+  units = StringField('Unit of Measurement', validators=[Length(min=0, max=10)])
+  tags = MultiCheckboxField("Categories", choices=TRAIT_CATEGORY_OPTIONS, validators=[Required()])
+  username = StringField('Username', validators=[Required(), Length(min=3, max=50)])
+  institution = StringField('Institution', validators=[Required(), Length(min=3, max=50)])
+  source_lab = StringField('Source Lab', validators=[Required(), Length(min=1, max=4)])
+  protocols = TextAreaField('Protocols', validators=[Length(min=0, max=200)])
+  publication = TextAreaField('Publications', validators=[Length(min=0, max=200)])
 
 
 
