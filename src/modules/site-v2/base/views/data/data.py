46664679ffd35604c25ff0f45cca376ccf1ec64d
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2024-05-31 17:42:53 -0500

    Safer check for trait owner

--- src/modules/site-v2/base/views/data/data.py
+++ src/modules/site-v2/base/views/data/data.py
@@ -20,7 +20,7 @@ from caendr.utils.local_files      import LocalUploadFile
 from caendr.utils.env              import get_env_var
 from caendr.utils.data             import get_file_format
 from base.utils.auth               import jwt_required, get_current_user, user_is_admin
-from base.utils.trait              import add_trait, update_trait_metadata, user_is_trait_owner
+from base.utils.trait              import add_trait, update_trait_metadata
 from base.utils.view_decorators    import parse_trait
 from base.forms                    import TraitSubmissionForm, EmptyForm
 from constants                     import TOOL_INPUT_DATA_VALID_FILE_EXTENSIONS
@@ -210,7 +210,6 @@ def trait(trait: Trait):
     'file_content':  phenotype_values,
     'form':          EmptyForm(),
     'user_is_owner': trait.file.belongs_to_user(get_current_user()),
-    # 'user_is_owner': user_is_trait_owner(trait.file.serialize(), get_current_user()),
 
     'reviewing': reviewing,
     'help_email': SITE_OWNER['email'],
@@ -225,8 +224,9 @@ def edit_trait(id):
     Edit Trait Page
   """
   user = get_current_user()
-  trait_ds = TraitFile.get_ds(id).serialize(include_name=True)
-  if user_is_trait_owner(trait_ds, user) and not user_is_admin():
+  trait_file = TraitFile.get_ds(id)
+  trait_ds = trait_file.serialize(include_name=True)
+  if trait_file.belongs_to_user(user) and not user_is_admin():
     return abort(401)
   
   # Handle Trait Update
@@ -331,7 +331,7 @@ def download_trait_file(id):
   """ Download the trait file """
   user = get_current_user()
   trait_ds = TraitFile.get_ds(id)
-  if user_is_trait_owner(trait_ds.serialize(), user) and not user_is_admin():
+  if trait_ds.belongs_to_user(user) and not user_is_admin():
     return abort(401)
   
   file = download_blob_to_file(MODULE_DB_OPERATIONS_BUCKET_NAME, trait_ds.get_filepath()[1], destination=UPLOADS_DIR)
