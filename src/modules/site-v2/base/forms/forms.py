d2c9a75e43eb92a7894cc42630342272f32c4487
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2024-07-08 15:59:13 -0500

    Track edit form styling & layout

--- src/modules/site-v2/base/forms/forms.py
+++ src/modules/site-v2/base/forms/forms.py
@@ -212,7 +212,7 @@ class AdminEditBrowserTrackForm(FlaskForm):
 
   display_name  = StringField('Display Name', [Length(min=3, max=200)])
   filename      = StringField('Filename', [Length(min=3, max=200)])
-  checked       = BooleanField('Checked by Default')
+  checked       = BooleanField('Checked by Default in Genome Browser')
 
   availability  = MultiCheckboxField('Availability in Genome Browser', choices=_AVAILABILITY)
   modify_availability  = BooleanField()
