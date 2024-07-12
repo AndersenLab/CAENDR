9902e42c74b0fba769c373296debd19a4f63d037
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2024-07-12 10:46:37 -0500

    Add trailing slash to filepaths

--- src/modules/site-v2/base/views/admin/gene_browser_tracks.py
+++ src/modules/site-v2/base/views/admin/gene_browser_tracks.py
@@ -41,7 +41,7 @@ def admin_gene_browser_tracks():
     },
 
     'form': AdminEditBrowserTrackForm(),
-    'file_location': join_path(BrowserTrackDefault.release_bucket(), BrowserTrackDefault.release_prefix().raw_string),
+    'file_location': join_path(BrowserTrackDefault.release_bucket(), BrowserTrackDefault.release_prefix().raw_string) + '/',
     'GENOME_BROWSER_TOOLS': GENOME_BROWSER_TOOLS,
   })
 
@@ -76,7 +76,7 @@ def edit_page(track: BrowserTrackDefault = None):
     'title': ('Edit' if track else 'Create') + ' Browser Track',
     'form':  form,
     'track': track,
-    'file_location': join_path(BrowserTrackDefault.release_bucket(), BrowserTrackDefault.release_prefix().raw_string),
+    'file_location': join_path(BrowserTrackDefault.release_bucket(), BrowserTrackDefault.release_prefix().raw_string) + '/',
     'filepath': track.get_filepath_template(BlobURISchema.HTTPS).raw_string if track else '',
   })
 
