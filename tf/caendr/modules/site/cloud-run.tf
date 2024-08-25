79c455baccffa17901a1be95bd1166b01276e938
Orzu Tursunova <orzut@github.com>
2024-08-25 11:15:13 -0500

    Set up gunicorn timeout in Dockerfile

--- tf/caendr/modules/site/cloud-run.tf
+++ tf/caendr/modules/site/cloud-run.tf
@@ -29,7 +29,6 @@ resource "google_cloud_run_service" "site" {
     spec {
       containers {
         image = data.google_container_registry_image.module_site.image_url
-        timeout_seconds = 4800
         dynamic "env" {
           for_each = local.envs
           content {
